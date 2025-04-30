c6305a31b20f93cd23cb654a83c50fe01e6c4b49
AntKazakovv <propaneld@protonmail.com>
2025-04-30 16:30:05 +0000

    -

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 import alpinejs from "https://cdn.skypack.dev/alpinejs" 
 
-console.log(alpinejs);
 
 alpinejs.data('personalInfo', () => ({
     firstName: "Anton",
     lastName: "Kazakov",
-    birthday: (6, 1, 1997),
+    birthday: [6, 1, 1997],
+    race: "Human",
+    sex: "Sexy",
+    
  
     get age() {
         const currentDate = new Date();
         const currentYear = currentDate.getFullYear();
+        console.log(this.birthday);
         return currentYear - this.birthday[2];
     },
 
